cd7e60b8f26f8c688a640d36b783ec6afb6eca74
Sang Han <jjangsangy@gmail.com>
2016-06-29 17:36:58 -0700

    removed lookup dictionary

--- ccjob/structures.py
+++ ccjob/structures.py
@@ -77,21 +77,3 @@ class CaseInsensitiveDict(MutableMapping):
     # Copy is required
     def copy(self):
         return CaseInsensitiveDict(self._store.values())
-
-class LookupDict(dict):
-    """Dictionary lookup object."""
-
-    def __init__(self, name=None):
-        self.name = name
-        super(LookupDict, self).__init__()
-
-    def __repr__(self):
-        return '<lookup \'%s\'>' % (self.name)
-
-    def __getitem__(self, key):
-        # We allow fall-through here, so values default to None
-
-        return self.__dict__.get(key, None)
-
-    def get(self, key, default=None):
-        return self.__dict__.get(key, default)
