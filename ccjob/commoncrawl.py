699458f9cdc795117f11cf198cf1a0fa5a7d007e
ndsteve <cwings47@gmail.com>
2017-07-04 17:10:14 -0400

    Update commoncrawl.py

--- ccjob/commoncrawl.py
+++ ccjob/commoncrawl.py
@@ -93,7 +93,7 @@ class CommonCrawl(MRJob):
                 start = 0
             end = start + len(self.pattern)
             end = end + 50
-            yield str(start)+" "+str(len(self.pattern))+" "+str(end)
+            yield body[start:end]
 
     def reducer(self, url, values):
         yield (url[0], url[1])
